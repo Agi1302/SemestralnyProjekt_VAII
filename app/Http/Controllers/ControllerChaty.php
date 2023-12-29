3fdee2b51a533618af1b9bfd677c23ad5f4f37fa
Agi1302 <agatapavlikova1@gmail.com>
2023-12-29 21:55:09 +0100

    Prvotne zobrazenie viewChaty

--- app/Http/Controllers/ControllerChaty.php
+++ app/Http/Controllers/ControllerChaty.php
@@ -23,13 +23,13 @@ class ControllerChaty extends Controller
             'nazov' => 'required|max:200',
             'text' => 'required|max:1000',
             'obrazok' => 'required|max:200',
-            'url' => 'required|max:1000',
+            'url' => 'required|max:5000',
         ],
             [
                 'nazov.max' => 'Obrazok môže mať maximálne 200 znakov',
                 'text.max' => 'Položka text vrcholu je povinná, maximalny pocet prekroceny',
                 'obrazok.max' => 'Obrazok môže mať maximálne 200 znakov',
-                'url.max' => 'URL môže mať maximálne 1000 znakov',
+                'url.max' => 'URL môže mať maximálne 5000 znakov',
             ]);
 
         $chata = new Chata;
@@ -96,13 +96,13 @@ class ControllerChaty extends Controller
             'nazov' => 'required|max:200',
             'text' => 'required|max:1000',
             'obrazok' => 'required|max:200',
-            'url' => 'required|max:1000',
+            'url' => 'required|max:5000',
         ],
             [
                 'nazov.max' => 'Obrazok môže mať maximálne 200 znakov',
                 'text.max' => 'Položka text vrcholu je povinná, maximalny pocet prekroceny',
                 'obrazok.max' => 'Obrazok môže mať maximálne 200 znakov',
-                'url.max' => 'URL môže mať maximálne 1000 znakov',
+                'url.max' => 'URL môže mať maximálne 5000 znakov',
             ]);
 
 
--- resources/views/viewChaty.blade.php
+++ resources/views/viewChaty.blade.php
@@ -10,46 +10,56 @@
     </div>
 
     <div class="container mt-5">
-        <div class="row justify-content-center">
-
-
-
-
+        <div class="row justify-content-center odstavec">
             @foreach($chaty as $chata)
+                <div class="container">
 
-
-                <div class="class=textyFerrat" style="margin-bottom: 20px;">
                     <div class="row text-center mb-3 prazdnyPosun"></div>
-                    <div class="row align-items-center odstavec">
-                        <div class="col-md-5 mb-5 d-flex align-items-center ">
+
+                    <div class="row">
+                        <div class="col-12 col-md-5">
+                            <div class="row-2" style="text-align: center;">
+                                <p class="mt-2 kartickaNadpisy "></p>
+                                <p class="mt-2 hlavnyNadpisChaty">{{$chata->nazov}}</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-7">
+                            <iframe src="{{ $chata->url }}" class="mapaChaty" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 col-md-6 medzera">
                             <img src="{{asset('Obrazky/Chaty/'.$chata->obrazok)}}" class="img-fluid obrazokChaty" alt="Popis">
-
                         </div>
-                        <div class="col-md-7 mb-7 d-flex align-items-center">
-                            <div class="row-2">
-                                <p class="mt-2 kartickaNadpisy"></p>
-                                <p class="mt-2">{{ $chata->nazov}}</p>
-                            </div>
-                        </div>
-                        <div class="col-md-7 mb-7 d-flex align-items-center">
-                            <div class="row-2">
-                                <p class="mt-2 kartickaNadpisy"></p>
-                                <p class="mt-2">{{ $chata->text}}</p>
-                            </div>
-                        </div>
-
-                        <div class="col-md-7 mb-7">
-                            <div class="row-2">
-                                <p class="mt-2">{{ $chata->url}}</p>
-                            </div>
-                        </div>
-
-
-                        <div class="col-md-5 mb-5">
-                            <img src="Obrazky/Ferraty/ferrataDveVeze.jpg" class="img-fluid rounded-start obrazokFerrataMartin  mx-auto d-block" alt="...">
+                        <div class="col-12 col-md-6 medzera">
+                            <p class="mt-2 kartickaNadpisy"></p>
+                            <p class="mt-2 textChaty">{{ $chata->text}}</p>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
+    </div>
+
 @endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
